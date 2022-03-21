f55e10b4df1690a4c42c999c41e1594d33e8a516
Preston Smith <preston.s1993@gmail.com>
2022-03-21 00:23:08 -0500

    Typo

--- src/com/dexecrator/Dexecrator.java
+++ src/com/dexecrator/Dexecrator.java
@@ -14,7 +14,7 @@
  *	You should have received a copy of the GNU General Public License
  *	along with Dexecrator.  If not, see <http://www.gnu.org/licenses/>.
  *
- *	Authors: see <https://github.com/DalvikVM/Dexecrator>
+ *	Authors: see <https://github.com/DalvikJVM/Dexecrator>
  */
 
 package com.dexecrator;
